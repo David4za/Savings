046763b66c9e603866a6f764894d91f301cca11d
Dawid van der Merwe <dawid@Dawids-MacBook-Pro-M1.local>
2025-11-27 21:38:20 +0100

    First big change in months. Removed streamlit_app.py and moved to main. Added uv. Added a lot of small elements to input_plan.py, mainly by USER INPUTS. Updated fig y axis to only show what is included. Spinner by button is something I am still thinking about

--- pages/input_plan.py
+++ pages/input_plan.py
@@ -2,21 +2,61 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+import time
 from millify import millify
 
-st.title("Savings Plan")
+st.set_page_config(
+    page_title="Future Savings Planner",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("Future Savings Planner")
+st.caption("Play with monthly savings, lumpsums, interest rates etc to see how your wealth can grow.")
 
 # ---- USER INPUTS ----
-col1, col2, col3 = st.columns(3)
+with st.container():
+    st.markdown("### Assumptions")
+    st.write("Adjust the inputs to model your future savings")
+    with st.expander("Investment Inputs", expanded=True):
+        col1, col2, col3 = st.columns(3)
 
-with col1:
-    P = st.number_input("Fixed amount to save per month", value=400, min_value = 0, )
-    L = st.number_input("Lump Sum Amount", min_value=0)
-with col2:    
-    r = st.number_input("Annual expected interest rate", value=5, min_value = 0)
-    t = st.number_input("Number of years", value=30, min_value = 0)
-with col3:
-    inflation = st.number_input("Annual inflationrate", value=2,  min_value= 1)
+        with col1:
+            P = st.number_input(
+                "Fixed amount to save per month", 
+                value=400,
+                min_value = 0,
+                step=50,
+                help="How much do you plan to save monthly"
+                )
+            L = st.number_input(
+                "Lump Sum Amount", 
+                min_value=0,
+                step=500,
+                help="Any amount you want to also add in the investment pool"
+                )
+        with col2:    
+            r = st.number_input(
+                "Annual expected interest rate (%)", 
+                value=7,
+                min_value = 0,
+                max_value=30,
+                help="Average yearly return (currently roughly 7%)"
+                )
+            t = st.number_input(
+                "Number of years",
+                value=30,
+                min_value = 0,
+                help="Number years you are planning to save"
+                )
+        with col3:
+            inflation = st.number_input(
+                "Annual inflationrate (%)", 
+                value=2,
+                min_value=0,
+                max_value=15,
+                help="Used to calculate inflation-adjust value (Governemnts aim for 2% p/y)"
+                )
 
 # ---- The Math -----
 
@@ -52,7 +92,10 @@ def inflation_adjustment(total_savings: float, inflation_rate: float, t: int) ->
     return total_savings / ((1 + infla_rate)**t)
 
 # ---- Calculations and Display ----
-if st.button("Calculate"):
+if st.button("Calculate", type="primary"):
+    with st.spinner("Calculating ..."):
+        time.sleep(1)
+        st.success("Calculations complete!")
     df = future_value_annuity(P=P, L=L, r=r, t=t)
     if not df.empty:
         total_fv = df.loc[df.index[-1], "Total FV"]
@@ -64,18 +107,36 @@ if st.button("Calculate"):
                                                 t=t)
 
     # ---- Metric Values ----
-        col_1, col_2, col_3, col_4 = st.columns(4)
-        col_1.metric("Total Savings", f"€{millify(total_fv)}")
-        col_2.metric("Total Contributions", f"{millify(total_contrib)}")
-        col_3.metric("Total Interest Earned", f"{millify(total_interest)}")
-        col_4.metric("Adjusted for Inflation", f"€{millify(adjusted_savings)}")
+        st.markdown("### Summary")
+        st.caption("Key figure overview at the end of the savings term")
+        with st.container():
+            col_1, col_2, col_3, col_4 = st.columns(4)
+            col_1.metric("Total Savings", f"€{millify(total_fv)}")
+            col_2.metric("Total Contributions", f"{millify(total_contrib)}")
+            col_3.metric("Total Interest Earned", f"{millify(total_interest)}")
+            col_4.metric("Adjusted for Inflation", f"€{millify(adjusted_savings)}")
 
     # ---- Graph ----
+        y_colums = ["Total FV"]
+        if not (df['FV Annuity'] == 0).all():
+            y_colums.append("FV Annuity")
+        if not (df['FV Lump Sum'] == 0).all():
+            y_colums.append("FV Lump Sum")
+        
         fig_1 = px.line(
             df,
             x="Year",
-            y=["FV Annuity", "FV Lump Sum", "Total FV"],
+            y=y_colums,
             markers=True,
-            title="Savings Over Time")
+            title="Savings Over Time",
+            labels={
+                "Year":"Year",
+                "value":"Amount (€)",
+                "variable": "Component"   
+            })
+        fig_1.update_layout(
+            template="plotly_white",
+            margin=dict(l=40, r=20, t=60, b=40),
+            )
         
         st.plotly_chart(fig_1, use_container_width=True)
